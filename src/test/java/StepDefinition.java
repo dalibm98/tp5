e7c070cc8cc7943f08d9b03bcd8f6d658be5676f
Ben Mariem Mohamed Ali <74265428+dalibm98@users.noreply.github.com>
2023-11-21 21:43:18 +0100

    Update StepDefinition.java

--- src/test/java/StepDefinition.java
+++ src/test/java/StepDefinition.java
@@ -1,11 +1,8 @@
-
 import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
-
-import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import io.cucumber.java.Before;
@@ -15,13 +12,13 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
 @RunWith(Cucumber.class)
-
 public class StepDefinition {
     public WebDriver driver;
     public FirefoxBinary firefoxBinary;
 
- @Before
+    @Before
     public void createWebDriver() {
+        // Créer une instance de FirefoxBinary pour spécifier le chemin du binaire Firefox
         firefoxBinary = new FirefoxBinary();
         firefoxBinary.addCommandLineOptions("--headless", "--no-sandbox");
     }
@@ -33,29 +30,37 @@ public class StepDefinition {
         }
     }
 
-    @Given("I open the Google search page")
+    @Given("J'ouvre la page de recherche Google")
     public void i_open_google_search_page() {
+        // Définir le chemin du driver Gecko
         System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 
-        // Use the full path to the Firefox executable
+        // Utiliser le chemin complet vers l'exécutable Firefox
         String firefoxPath = "/mnt/c/Program Files/Mozilla Firefox/firefox.exe";
         System.setProperty("webdriver.firefox.bin", firefoxPath);
 
+        // Configurer les options de Firefox
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
 
+        // Initialiser le pilote Firefox
         driver = new FirefoxDriver(firefoxOptions);
+
+        // Ouvrir la page Google
         driver.get("https://www.google.com");
     }
-    @When("lookup the word {string}")
+
+    @When("je recherche le mot {string}")
     public void lookup_the_word(String string) {
+        // Effacer le champ de recherche, saisir le mot et cliquer sur le bouton de recherche
         driver.findElement(By.name("q")).clear();
         driver.findElement(By.name("q")).sendKeys(string);
         driver.findElement(By.name("btnk")).click();
     }
-    @Then("Serach results disppalt {string}")
-    public void serach_results_disppalt(String string) {
-        System.out.println(driver.getTitle().contains(string));
 
+    @Then("les résultats de la recherche affichent {string}")
+    public void search_results_display(String string) {
+        // Afficher si le titre de la page contient la chaîne spécifiée
+        System.out.println(driver.getTitle().contains(string));
     }
 }
