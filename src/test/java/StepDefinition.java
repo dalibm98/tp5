ae8aa9c586e2da2595def70b4aca6a2b73911306
Ben Mariem Mohamed Ali <74265428+dalibm98@users.noreply.github.com>
2023-11-21 15:37:23 +0100

    Update StepDefinition.java

--- src/test/java/StepDefinition.java
+++ src/test/java/StepDefinition.java
@@ -36,7 +36,7 @@ public class StepDefinition {
 
         firefoxBinary.addCommandLineOptions("--headless", "--no-sandbox");
         System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
-        System.setProperty("webdriver.firefox.bin", "C:/Program Files/Firefox Developer Edition/firefox.exe");
+System.setProperty("webdriver.firefox.bin", "/mnt/c/Program Files/Firefox Developer Edition/firefox.exe");
 
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
